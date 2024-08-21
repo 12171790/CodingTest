6457b2997a15e513e16457a764cdc6b8ac962110
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-08-21 15:28:41 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = money;
+
+    for (int i = 1; i <= count; i++) {
+        answer -= price * i;
+    }
+    
+    if (answer > 0) {
+        answer = 0;
+    } else {
+        answer = abs(answer);
+    }
+    
+    return answer;
+}
