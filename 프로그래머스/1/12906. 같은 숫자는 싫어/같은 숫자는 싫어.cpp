f57df2db609cfd4b9def53909139320e92b77267
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-08-21 18:24:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.48 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    int prev = -1;
+    
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] != prev) {
+            answer.emplace_back(arr[i]);
+            prev = arr[i];
+        }
+    }
+    
+    return answer;
+}
