899450e1a13ea7b567a2accfcfcb29bc4c3ccdbb
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-08-28 14:18:55 +0900

    [level 2] Title: 피보나치 수, Time: 0.33 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int num1 = 0;
+    int num2 = 1;
+    int fib = 0;
+    
+    for (int i = 2; i <= n; i++) {
+        fib = (num1 + num2) % 1234567;
+        int temp = num1;
+        num1 = num2;
+        num2 = fib;
+    }
+    
+    answer = fib % 1234567;
+    
+    return answer;
+}
