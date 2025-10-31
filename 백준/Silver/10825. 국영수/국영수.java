ddd4f056b5ac96071d78db485625674ab2abb654
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-10-31 13:59:50 +0900

    [Silver IV] Title: 국영수, Time: 1204 ms, Memory: 65216 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -2,58 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  static class Student implements Comparable<Student> {
-    public String name;
-    public int kor;
-    public int eng;
-    public int math;
-
-    public Student(String name, int kor, int eng, int math) {
-      this.name = name;
-      this.kor = kor;
-      this.eng = eng;
-      this.math = math;
-    }
-
-    @Override
-    public int compareTo(Student other) {
-      if (this.kor != other.kor) {
-        return other.kor - this.kor;
-      } else {
-        if (this.eng != other.eng) {
-          return this.eng - other.eng;
-        } else {
-          if (this.math != other.math) {
-            return other.math - this.math;
-          } else {
-            return this.name.compareTo(other.name);
-          }
+    
+    static class Student implements Comparable<Student> {
+        public String name;
+        public int kor;
+        public int eng;
+        public int math;
+        
+        public Student(String name, int kor, int eng, int math) {
+            this.name = name;
+            this.kor = kor;
+            this.eng = eng;
+            this.math = math;
         }
-      }
+        public int compareTo(Student other) {
+                if (this.kor != other.kor) {
+                    return other.kor - this.kor;
+                } else if (this.eng != other.eng) {
+                    return this.eng - other.eng;
+                } else if (this.math != other.math) {
+                    return other.math - this.math;
+                } else {
+                    return this.name.compareTo(other.name);
+                }
+            }
+        }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Student[] students = new Student[n];
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int kor = Integer.parseInt(st.nextToken());
+            int eng = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
+            
+            Student temp = new Student(name, kor, eng, math);
+            students[i] = temp;
+        }
+        
+        Arrays.sort(students);
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println(students[i].name);
+        }
     }
-  }
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    Student[] students = new Student[n];
-
-    for (int i = 0; i < n; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      String name = st.nextToken();
-      int[] scores = new int[3];
-
-      for (int j = 0; j < 3; j++) {
-        scores[j] = Integer.parseInt(st.nextToken());
-      }
-
-      students[i] = new Student(name, scores[0], scores[1], scores[2]);
-    }
-
-    Arrays.sort(students);
-
-    for (Student st : students) {
-      System.out.println(st.name);
-    }
-  }
 }
