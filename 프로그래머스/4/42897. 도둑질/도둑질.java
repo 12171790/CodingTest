3f8baaacab6bb48a6baaae1975f83a72f93bb522
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-11-13 23:04:52 +0900

    [level 4] Title: 도둑질, Time: 23.83 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] money) {
+        int answer = 0;
+        int n = money.length;
+        int[] dp = new int[n];
+        
+        dp[0] = 0;
+        dp[1] = money[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+        
+        int temp = dp[n - 1];
+        
+        dp[0] = money[0];
+        dp[1] = money[0];
+        
+        for (int i = 2; i < n - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+                
+        
+        return Math.max(temp, dp[n - 2]);
+    }
+}
