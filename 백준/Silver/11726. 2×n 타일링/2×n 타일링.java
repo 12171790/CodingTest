e68b3730f3bd0e188b1fdaa0c6e2dc0cfe2270a8
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-11-25 22:38:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,31 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[][] arr = new int[1005][3];
-    
-    static void func(int k) {
-        arr[k][1] = (arr[k - 1][1] + arr[k - 1][2]) % 10007;
-        arr[k][2] = (arr[k - 2][1] + arr[k - 2][2]) % 10007;
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        arr[1][1] = 1;
-        arr[1][2] = 0;
-        arr[2][1] = 1;
-        arr[2][2] = 1;
+        // dp[n] = dp[n - 1] + dp[n - 2]
+        int[] dp = new int[n];
         
-        // n이 짝수인 경우
-        // arr[n][1] = arr[n - 1][1] + arr[n - 1][2]
-        // arr[n][2] = arr[n - 2][1] + arr[n - 2][2]
-        
-        for (int i = 3; i <= n; i++) {
-            func(i);
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        if (n == 2) {
+            System.out.println(2);
+            return;
         }
         
-        System.out.println((arr[n][1] + arr[n][2]) % 10007);
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for (int i = 2; i < n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        
+        System.out.println(dp[n - 1]);
     }
 }
