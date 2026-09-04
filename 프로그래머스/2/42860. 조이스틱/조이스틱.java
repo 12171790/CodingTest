3e6de1dcdcf410cf13aac1ea6f496c6fbd68ad7b
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2026-09-04 13:15:33 +0900

    [level 2] Title: 조이스틱, Time: 0.15 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,83 +1,77 @@
+import java.util.*;
+
 class Solution {
     public int solution(String name) {
-            int answer = Integer.MAX_VALUE;
-            int[] gapArr = new int[name.length()];
+        int answer = Integer.MAX_VALUE;
+        int alpExcludeACnt = 0;
+
+        List<Integer> upDownCntList = new ArrayList<>();
+        for (char c : name.toCharArray())
+        {
             int cnt = 0;
-            // A B C D E F G H I J K L M N(+13) O(-12) P Q R S T U W V X Y Z
-            // O부터는 -가 더 빠름.
-            for (int i = 0 ; i < name.length(); i++)
+            if (c >= 'O')
             {
-                char c = name.charAt(i);
-                int gap = 0;
-                if (c == 'A') continue;
+                cnt = 'Z' - c + 1;
+            }
+            else
+            {
+                cnt = c - 'A';
+            }
 
-                if (c >= 'O')
-                {
-                    gap = ('Z' - c) + 1;
-                }
-                else
-                {
-                    gap = (c - 'A');
-                }
-                cnt++;
+            if (cnt != 0) alpExcludeACnt++;
 
-                gapArr[i] = gap;
+            upDownCntList.add(cnt);
+        }
+
+        for (int i = 0; i < upDownCntList.size(); i++)
+        {
+            // 오른쪽으로 i까지 이동 후 왼쪽으로 이동
+            int move = 1; // 이동 방향
+            int curIdx = 0; // 현재 위치
+            int totalCnt = 0; // 총 조이스틱 조작 횟수
+            int metAlpExcludeACnt = 0; // A를 제외한 알파벳을 만난 횐수
+            boolean[] visited = new boolean[upDownCntList.size()]; // 방문한 위치 표시
+            while (metAlpExcludeACnt != alpExcludeACnt)
+            {
+                if (curIdx == i) move *= -1;
+                if (curIdx < 0) curIdx = name.length() - 1;
+                if (!visited[curIdx] && upDownCntList.get(curIdx) != 0)
+                {
+                    metAlpExcludeACnt++;
+                    totalCnt += upDownCntList.get(curIdx);
+                }
+                                
+                totalCnt++;
+                visited[curIdx] = true;
+                curIdx += move;
             }
             
-            if (cnt == 0) return 0;
+            answer = Math.min(answer, totalCnt > 0 ? totalCnt - 1 : totalCnt);
             
-            for (int i = 0; i < gapArr.length; i++)
+            // 왼쪽으로 i까지 이동 후 오른쪽으로 이동
+            move = -1; // 이동 방향
+            curIdx = name.length() - 1; // 현재 위치
+            totalCnt = 0; // 총 조이스틱 조작 횟수
+            metAlpExcludeACnt = 0; // A를 제외한 알파벳을 만난 횐수
+            visited = new boolean[upDownCntList.size()]; // 방문한 위치 표시
+            
+            while (metAlpExcludeACnt != alpExcludeACnt)
             {
-                int temp = cnt;
-                int moveCnt = -1;
-                int idx = 0; // 현재 위치
-                int move = 1;
-                boolean[] visited = new boolean[gapArr.length];
-
-                // i 지점에서 왼쪽으로 진행
-                while (temp > 0)
+                if (curIdx == i) move *= -1;
+                if (curIdx >= name.length()) curIdx = 0;
+                if (!visited[curIdx] && upDownCntList.get(curIdx) != 0)
                 {
-                    if (idx < 0) idx = gapArr.length - 1;
-                    if (idx >= gapArr.length) idx = 0;
-                    if (!visited[idx] && gapArr[idx] != 0)
-                    {
-                        temp--;
-                        moveCnt += gapArr[idx];
-                        visited[idx] = true;
-                    }
-                    if (idx == i) move = -1;
-
-                    moveCnt++;
-                    idx += move;
+                    metAlpExcludeACnt++;
+                    totalCnt += upDownCntList.get(curIdx);
                 }
 
-                answer = Math.min(answer, moveCnt);
-
-                temp = cnt;
-                move = -1;
-                idx = 0;
-                visited = new boolean[gapArr.length];
-                moveCnt = -1;
-
-                // i 지점에서 오른쪽으로 진행
-                while(temp > 0)
-                {
-                    if (idx < 0) idx = gapArr.length - 1;
-                    if (idx >= gapArr.length) idx = 0;
-                    if (!visited[idx] && gapArr[idx] != 0)
-                    {
-                        temp--;
-                        moveCnt += gapArr[idx];
-                        visited[idx] = true;
-                    }
-                    if (idx == i) move = 1;
-
-                    moveCnt++;
-                    idx += move;
-                }
-
-                answer = Math.min(answer, moveCnt);
+                visited[curIdx] = true;
+                curIdx += move;
+                totalCnt++;
             }
+
+            answer = Math.min(answer, totalCnt);
+        }
         
         return answer;
     }
